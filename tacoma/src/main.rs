a6e0d0bcb27551abe500beaec3e355ee325a5886
Nathan Whitehead <nwhitehe@gmail.com>
2026-05-23 12:16:39 -0700

    pink

--- tacoma/src/main.rs
+++ tacoma/src/main.rs
@@ -4,8 +4,11 @@ use anyhow::{Result, bail};
 use num_traits::float::FloatConst;
 use noise::{NoiseFn, Perlin, Seedable};
 
-fn prob(x: f32, y: f32) -> f32 {
-    x * y
+fn prob(perlin: &Perlin, x: f32, y: f32) -> f32 {
+    let p1 = (perlin.get([(x * 3.0) as f64, (y * 3.0) as f64]) as f32 + 1.0) * 0.5;
+    let p2 = (perlin.get([(x * 10.0) as f64, (y * 10.0) as f64]) as f32 + 1.0) * 0.5;
+    let p3 = (perlin.get([(x * 20.0) as f64, (y * 20.0) as f64]) as f32 + 1.0) * 0.5;
+    (p1 + p2 * 0.5 + p3 * 0.25) - 0.3
 }
 
 fn draw_strand(pixmap: &mut Pixmap, length: f32, width: f32, x: f32, y: f32, angle: f32, angle_delta: f32, color: Color) -> Result<()> {
@@ -18,7 +21,6 @@ fn draw_strand(pixmap: &mut Pixmap, length: f32, width: f32, x: f32, y: f32, ang
     let Some(path) = ({
         let mut pb = PathBuilder::new();
         pb.move_to(x, y);
-        //pb.line_to(x1, y1);
         pb.quad_to(x1, y1, x2, y2);
         pb.finish()
     }) else {
@@ -46,15 +48,14 @@ fn main() -> Result<()> {
     ) else {
         bail!("pixmap failed");
     };
-    pixmap.fill(Color::BLACK);
+    pixmap.fill(Color::from_rgba8(102, 70, 75, 255));
     let perlin = Perlin::new(1);
-    let s = 2.0;
-    for i in 0..5000 {
+    for i in 0..20000 {
         let (mut x, mut y) = (0.0, 0.0);
         while true {
             (x, y) = (rng.next_f32(), rng.next_f32());
             //let p = prob(x, y);
-            let p = (perlin.get([(x * s) as f64, (y * s) as f64]) as f32 + 1.0) * 0.5;
+            let p = prob(&perlin, x, y);
             let e = rng.next_f32();
             if e < p {
                 break;
@@ -63,7 +64,7 @@ fn main() -> Result<()> {
         //let angle = rng.next_f32() * 2.0 * std::f32::consts::PI;
         let angle = 1.8 + rng.next_f32() * 1.0;
         let angle_delta = rng.next_f32() - 0.5;
-        draw_strand(&mut pixmap, 0.01, 0.001, x, y, angle, angle_delta, Color::WHITE)?;
+        draw_strand(&mut pixmap, 0.005, 0.0005, x, y, angle, angle_delta, Color::WHITE)?;
     }
     pixmap.save_png("out.png")?;
 
