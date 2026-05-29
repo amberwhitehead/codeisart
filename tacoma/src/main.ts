9eec3b38cf6b78aadef46ca2436ab09d2306dc37
Nathan Whitehead <nwhitehe@gmail.com>
2026-05-28 18:04:45 -0700

    initial shader

--- tacoma/src/main.ts
+++ tacoma/src/main.ts
@@ -1,2 +1,10 @@
+import GlslCanvas from 'glslCanvas';
+import frag from './frag.glsl?raw';
 
-console.log('Hello shaders');
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('Hello shaders', GlslCanvas);
+    const canvas = document.getElementById("glslCanvas");
+    const sandbox = new GlslCanvas(canvas);
+    sandbox.load(frag);
+    document.body.appendChild(canvas);
+});
